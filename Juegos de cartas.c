bd00694321c3e4e110f641788346c296ecf698f4
Cuco1897 <32716183+Cuco1897@users.noreply.github.com>
2017-10-12 13:58:08 -0500

    Update Juegos de cartas.c

--- Juegos de cartas.c
+++ Juegos de cartas.c
@@ -1,1 +1,43 @@
+//Juego 21 balckjack
 
+#include <stdio.h>
+
+
+int main (){
+	
+	int cartas, suma = 0, jugar, tubaraja[8], aux, baraja[56], mesa;
+	
+	
+do{
+	printf("\nRepartiendo cartas .........");
+
+	srand (time (NULL));
+	int valor = 1 + rand()/(rand-12/(12-1+1) +1 );
+	
+	do{
+		
+	printf("\n Tus cartas son: %d  %d  " ,baraja[0], baraja[1]);
+	aux = baraja[0] + baraja[1];
+
+	printf("\n Otra carta: (no = 0) (si = el resto de numeros)");
+	scanf ("%d",&cartas);
+	suma = aux;
+	suma = suma + baraja[0];
+	mesa = baraja[2] + baraja[3];
+	
+	}while(suma <= 21 || cartas = 0);
+
+	if (suma <= 21 && suma > mesa)
+	printf("\n Usted ha ganado ");
+	
+	if(suma <21)
+	printf("\n Has perdido");
+
+	printf("\n ¿Jugar otra vez? ");
+	scanf("%d",&jugar);
+	
+	
+}while (jugar != 0);
+
+	return 0;
+}
